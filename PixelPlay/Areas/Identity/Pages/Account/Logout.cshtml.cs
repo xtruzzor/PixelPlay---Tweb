c0e18b1f6a09f591d8b0cceee057cf8a8a631d7f
theos <mun.liviu03@gmail.com>
2024-05-30 23:48:44 +0300

    commit

--- PixelPlay/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ PixelPlay/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,10 +1,9 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,6 +11,7 @@ using Microsoft.Extensions.Logging;
 
 namespace PixelPlay.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -23,9 +23,14 @@ namespace PixelPlay.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
+        public void OnGet()
+        {
+        }
+
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
+            HttpContext.Session.SetString("roleName", "");
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
@@ -33,9 +38,7 @@ namespace PixelPlay.Areas.Identity.Pages.Account
             }
             else
             {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return Page();
             }
         }
     }
--- PixelPlay/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PixelPlay/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -29,21 +29,22 @@ namespace PixelPlay.Areas.Identity.Pages.Account
         private readonly IUserStore<IdentityUser> _userStore;
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger
+            )
+         
         {
             _userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
+          
         }
 
         /// <summary>
@@ -132,8 +133,7 @@ namespace PixelPlay.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
